ca651c41d0beaa37660e548b05a242cc1b99bf0e
Leonardo <leonardo@tokopedia.com>
2021-07-01 15:48:45 +0700

    add first gql query all pokemons

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module grpc_pokedex
 go 1.16
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/99designs/gqlgen v0.13.0
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/protobuf v1.5.2
+	github.com/vektah/gqlparser/v2 v2.1.0
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.27.0
 )
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,57 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"grpc_pokedex/graph/generated"
+	"grpc_pokedex/graph/model"
+	"log"
+
+	grpcModel "grpc_pokedex/model"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+)
+
+func servicePokedex() grpcModel.PokedexClient {
+
+	conn, err := grpc.Dial(":8000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect to :8000", err)
+	}
+
+	return grpcModel.NewPokedexClient(conn)
+
+}
+
+func (r *queryResolver) Pokemons(ctx context.Context) ([]*model.Pokemon, error) {
+	pokedex := servicePokedex()
+
+	var pokemons []*model.Pokemon
+
+	pokemonList, err := pokedex.GetPokemonList(context.Background(), new(empty.Empty))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	for _, val := range pokemonList.List {
+		var pokemon model.Pokemon
+
+		pokemon.ID = string(val.Id)
+		pokemon.Name = val.Name
+		pokemon.Height = int(val.Height)
+		pokemon.Weight = int(val.Weight)
+		pokemon.Types = &model.Type{TypeName: val.Name}
+
+		pokemons = append(pokemons, &pokemon)
+	}
+
+	return pokemons, nil
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
